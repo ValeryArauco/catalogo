ac6343a6f84106e09c040b5f58ff1e8b97b5de54
Tu Nombre <valery.arauco123@gmail.com>
2026-04-21 22:44:53 -0400

    Add metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="apple-mobile-web-app-title" content="Catalogo by V" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      </head>
       <body suppressHydrationWarning>{children}</body>
     </html>
   );
